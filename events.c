7a04401f925b3d38153bbc56fd4977027c269eb1
João <jocolognesi@gmail.com>
2025-01-28 15:03:28 -0300

    ready

--- events.c
+++ events.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcologne <jcologne@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jcologne <jcologne@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 18:49:19 by jcologne          #+#    #+#             */
-/*   Updated: 2025/01/20 17:32:26 by jcologne         ###   ########.fr       */
+/*   Updated: 2025/01/28 15:02:33 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 int	close_event(t_fractal *fractal)
 {
-	mlx_destroy_image(fractal->mlx, fractal->img.img_pointer);
-	mlx_clear_window(fractal->mlx, fractal->window);
-	mlx_destroy_display(fractal->mlx);
-	free(fractal->mlx);
-	exit (0);
+	if (fractal->img.img_pointer)
+		mlx_destroy_image(fractal->mlx, fractal->img.img_pointer);
+	if (fractal->window)
+		mlx_destroy_window(fractal->mlx, fractal->window);
+	if (fractal->mlx)
+	{
+		mlx_destroy_display(fractal->mlx);
+		free(fractal->mlx);
+	}
+	exit(0);
 }
+
 
 int	key_event(int key, t_fractal *fractal)
 {
